547ce180f4d44ea0a7f1585daed26d15a01614a5
Bibin Pulickal <bpulickal@woolworths.com.au>
2024-09-16 20:03:54 +0930

    Initial Commit

---
+++ RoPaSci/RoPaSciApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoPaSciApp.swift
+//  RoPaSci
+//
+//  Created by Bibin Jacob Pulickal on 16/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct RoPaSciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
